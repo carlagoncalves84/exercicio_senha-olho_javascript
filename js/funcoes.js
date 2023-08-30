9ae83ab787530564ebcfb2122628d3ffd6bd574c
Carla Goncalves <142905153+carlagoncalves84@users.noreply.github.com>
2023-08-30 15:02:55 +0200

    projeto

---
+++ js/funcoes.js
@@ -0,0 +1,6 @@
+function abrir(){
+    document.querySelector("#senha").setAttribute("type", "text");
+}
+function fechar(){
+    document.querySelector("#senha").setAttribute("type", "password");
+}
